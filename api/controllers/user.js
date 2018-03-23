5e26f1e05d56af8b413d4faed5c88ecd441c5ae7
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-23 12:20:13 -0600

    Setup post method on the '/api/users' route to create a user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,9 +2,18 @@ const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
-  const { username, password } = req.body;
+  const { username, passwordHash } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const user = new User(username, passwordHash);
+  user
+    .save()
+    .then(savedUser => {
+      res.status(200).json(savedUser);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'There was an error saving the user' })
+    });
 };
 
 module.exports = {
