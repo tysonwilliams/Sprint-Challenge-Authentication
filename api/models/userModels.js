2786a205ea252ff2619ef87bb59227981fd59300
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-23 11:34:38 -0600

    Setup User schema

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -8,6 +8,8 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: { type: String, required: true, unique: true, lowercase: true, },
+  password: { type: String, required: true, },
 });
 
 UserSchema.pre('save', function(next) {
